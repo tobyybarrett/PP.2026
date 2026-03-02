a4760c2918f852ccbc16e114e3c8324d0bebe3a4
tobyybarrett <tobybarrett2026@u.northwestern.edu>
2026-03-01 20:22:16 -0600

    Needed to do some work on the nav bar. wasn't looking great on mobile, no hamburger on nav bar. hopefully this helps.

---
+++ nav.js
@@ -0,0 +1,8 @@
+
+/*was having some trouble with the java script for the nav bar. looked online a bit, worked with Claude*/
+const hamburger = document.querySelector('.hamburger');
+const navButtons = document.querySelector('.nav.buttons');
+
+hamburger.addEventListener('click', () => {
+  navButtons.classList.toggle('nav-open');
+});
