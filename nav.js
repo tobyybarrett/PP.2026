d1464dcc4ca0a4315d7eb68baa43ac3ee13f6600
tobyybarrett <tobybarrett2026@u.northwestern.edu>
2026-03-01 20:36:33 -0600

    nav issues still
    s

--- nav.js
+++ nav.js
@@ -1,7 +1,7 @@
 
 /*was having some trouble with the java script for the nav bar. looked online a bit, worked with Claude*/
 const hamburger = document.querySelector('.hamburger');
-const navButtons = document.querySelector('.nav.buttons');
+const navButtons = document.querySelector('.nav-buttons');
 
 hamburger.addEventListener('click', () => {
   navButtons.classList.toggle('nav-open');
